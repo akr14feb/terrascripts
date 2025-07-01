264e129a44428a6fa103743afc331d695324990d
akr14feb <144898667+akr14feb@users.noreply.github.com>
2025-07-01 16:57:33 +0530

    Update onedeploy.sh

--- onedeploy.sh
+++ onedeploy.sh
@@ -1,6 +1,7 @@
 cd /var/lib/jenkins/workspace/oneClickDeploy
 rm -rf /var/lib/jenkins/workspace/oneClickDeploy/compute.tf
 rm -rf /var/lib/jenkins/workspace/oneClickDeploy/terraform*
+. /var/lib/jenkins/workspace/oneClickDeploy/vars.env
 terraform init
 terraform plan
 terraform apply -auto-approve
